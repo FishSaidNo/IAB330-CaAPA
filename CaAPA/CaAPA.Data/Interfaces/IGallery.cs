3d10d910defd5472b7f89ef7249203a4fe422e99
Paul David Stockwell <paul.stockwell@connect.qut.edu.au>
2015-10-27 22:55:09 +1000

    Added most of my work, not all of it and have not integrated it yet.

---
+++ CaAPA/CaAPA.Data/Interfaces/IGallery.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GalaSoft.MvvmLight.Views;
+using Xamarin.Forms;
+
+namespace CaAPA.Data
+{
+    public interface IGallery
+    {
+        void GetImageFromGallery();
+
+        void SetImageFromGallery(Image img);
+
+        event EventHandler<ImageSourceEventArgs> ImageSelected;
+    }
+
+    public class ImageSourceEventArgs : EventArgs
+    {
+        public ImageSourceEventArgs(Uri imageSource)
+        {
+            if (imageSource == null)
+                throw new ArgumentNullException("imagesource");
+            ImageSource = imageSource;
+        }
+        public Uri ImageSource { get; private set; }
+    }
+}
+
---
+++ CaAPA/CaAPA/Views/ImagePickerPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using CaAPA.Data.ViewModel;
+using CaAPA.Data;
+
+using Xamarin.Forms;
+
+namespace CaAPA
+{
+    public partial class ImagePickerPage : BaseView
+    {
+        public ImagePickerPage()
+        {
+            BindingContext = App.Locator.SamplePage;
+            InitializeComponent();
+            base.Init();
+            Title = "Pick a Image";
+            BackgroundColor = Color.FromRgb(255, 255, 255);
+        }
+
+        protected override void OnAppearing()
+        {
+            //Add this to all pages to avoid annoying icon next to the back button
+            var noIcon = "transparent1x1.png";
+            NavigationPage.SetTitleIcon(this, noIcon);
+        }
+    }
+}
